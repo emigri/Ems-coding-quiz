b0285b2db585ccde244c9bdb0673b52a3ce68a60
Your Name <you@example.com>
2024-03-03 23:06:45 +0000

    added questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,23 +2,28 @@
 // Questions contain buttons for each answer.
 var quizQuestions =
  [{
-    question:"Question-1", 
-    choices: ["1", "2", "3", "4"],
-    answer: '1',
+    question:"Which event occurs when the user clicks on an HTML element?", 
+    choices: ["onmouseover", "onclick", "onmouseclick", "onchange"],
+    answer: '<script>',
   },
   {
-    question:"Question-2", 
-    choices: ["a", "b", "c", "d"],
-    answer: 'a',
+    question:"How do you create a function in JavaScript?", 
+    choices: ["function myFunction()", "function:myFunction()", "function = myFunction()", "myFunction"],
+    answer: 'function myFunction()',
   },
   {
-    question:"Question-3", 
-    choices: ["w", "x", "y", "z"],
-    answer: 'z',
+    question:"How to write an IF statement in JavaScript?", 
+    choices: ["if (i == 5)", "if i = 5", "if i = 5 then", "if i == 5 then"],
+    answer: 'if (i == 5)',
   },
   {
-    question:"Question-4", 
-    choices: ["10", "9", "8", "7"],
-    answer: '9',
+    question:"How to write an IF statement for executing some code if 'i' is NOT equal to 5?", 
+    choices: ["if i=!5 then", "if (i<>5)", "if (i !=5)", "if i<>5"],
+    answer: 'if (i !=5)',
+  },  
+  {
+    question:"How does a FOR loop start?", 
+    choices: ["for (i=0, i<=5; i++)", "for (i <=5; i++)", "for i=1 to 5", "for (i=0;i <=5)"],
+    answer: 'for (i=0, i<=5; i++)',
   }  
 ] 
